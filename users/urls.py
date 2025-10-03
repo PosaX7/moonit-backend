c76cb09e2346ecbc6b2a7457a59e748b2ba336f1
PosaX7 <sunchung421@gmail.com>
2025-10-03 04:23:25 +0100

    Derniers changements backend

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
+from .views import RegisterView
 
 urlpatterns = [
-    path("login/", obtain_auth_token),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
